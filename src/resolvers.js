abcea51a4f1f5f9ba08215fc4c4eb63b55ed51e8
Derek Campanile <me@dpcamp.com>
2020-02-11 12:56:49 -0800

    Migration Complete

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,22 +1,305 @@
-//const model = require('../models');
+const sql = require('../models/index.js')
 
 const resolvers = {
-    Query: {
-        async User (root, { user_name }, { models }) {
-            console.log(user_name)
-              return models.users.findByPk(user_name, {include: [
-                        {model: models.phones},
-                        {model: models.pdq_computers, include: [
-                              {model: models.pdq_displays},
-                              {model: models.pdq_applications}
-                        ]},
-                        {model: models.service_requests},
-                  ]})
-        },
-        async allUsers(root, args, { models }) {
-              return models.users.findAll()
-        },
+      Query: {
+            /* -------- User Queries */
+            async User(root, { user_name }, { models }) {
+                  try {
+                        const userName = models.users.findByPk(user_name, {
+                              include: [
+                                    { model: models.phones },
+                                    {
+                                          model: models.pdq_computers, include: [
+                                                { model: models.pdq_displays },
+                                                { model: models.pdq_applications }
+                                          ]
+                                    },
+                                    { model: models.service_requests },
+                              ]
+                        })
+                        if(!userName){
+                              throw new Error(`${user_name} does not exist`)
+                        } else {
+                              return(userName)
+                        }
+                  } catch (err) {
+                        return (err)
+                  }
+
+            },
+            async allUsers(root, args, { models }) {
+                  try {
+                        return models.users.findAll(
+                              {
+                                    include: [
+                                          { model: models.phones },
+                                          {
+                                                model: models.pdq_computers, include: [
+                                                      { model: models.pdq_displays },
+                                                      { model: models.pdq_applications }
+                                                ]
+                                          },
+                                          { model: models.service_requests },
+                                    ]
+                              }
+                        )
+                  } catch (err) {
+                        return (err)
+                  }
+            },
+            async dfUser(root, { emp_id }, { models }) {
+                  try {
+                        const query =
+                              await models.sequelize.query(
+                                    `DayForceEmployees @EmpID=${emp_id}`,
+                                    {
+                                          raw: true,
+                                          type: models.sequelize.QueryTypes.SELECT
+                                    }
+                              )
+                        if(!query[0]){
+                              throw new Error(`No employee found with the folloing ID: ${emp_id}`)
+                        }
+                        else {
+                        return (query[0])
+                        }
+                  } catch (err) {
+                        return (err)
+                  }
+
+            },
+            /* -------- User Form Queries */
+            async allUserForms(root, args, { models }) {
+                  try {
+                        let queryParams = {};
+                        if (args.status) {
+                              queryParams = { status: args.status }
+                        }
+                        if (args.submitted_by) {
+                              queryParams = { submitted_by: args.submitted_by }
+                        }
+                        if (args.submitted_by && args.status) {
+                              queryParams = { status: args.status, submitted_by: args.submitted_by }
+                        }
+                        const pending_count = await models.user_form.count({
+                              where: { status: 'pending' }
+
+                        })
+                        const forms = await models.user_form.findAll({
+                              where: queryParams,
+                              include: [
+                                    {
+                                          model: models.users,
+                                          as: 'submit_user'
+                                    },
+                                    {
+                                          model: models.users,
+                                          as: 'create_user'
+                                    }
+                              ]
+                        })
+                        return { pending_count, forms }
+                  } catch (err) {
+                        return (err)
+                  }
+            },
+            async UserForm(root, { id }, { models }) {
+                  try {
+                        const pending_count = await models.user_form.count({
+                              where: { status: 'pending' }
+                        })
+                        const form = await models.user_form.findByPk(id)
+                        return { pending_count, form }
+                  } catch (err) {
+                        return (err)
+                  }
+            },
+            /* -------- Computer Queries */
+            async allComputers(root, args, { models }) {
+                  try {
+                        return models.pdq_computers.findAll()
+                  } catch (err) {
+                        return (err)
+                  }
+            },
+            async Computer(root, { computer_id }, { models }) {
+                  try {
+                        return models.pdq_computers.findOne({
+                              where: { computer_id: computer_id },
+                              include: [
+                                    { model: models.pdq_nic_ip },
+                                    { model: models.pdq_displays },
+                                    { model: models.pdq_applications },
+                                    { model: models.users }
+                              ]
+                        })
+                  } catch (err) {
+                        return (err)
+                  }
+            },
+            /* -------- Phones Queries */
+            async allPhones(root, args, { models }) {
+                  try {
+                        return models.phones.findAll({
+                              include: [{
+                                    model: models.users,
+                                    as: 'owners',
+                                    through: { attributes: [] }
+                              }]
+                        })
+                  } catch (err) {
+                        return (err)
+                  }
+            },
+            async Phone(root, args, { models }) {
+                  try {
+                        if (args.id) {
+                              queryParams = { id: args.id }
+                        }
+                        if (args.ext) {
+                              queryParams = { extension: args.ext }
+                        }
+                        await console.log(queryParams)
+                        const phone = await models.phones.findOne({
+                              where: queryParams,
+                              include: [{
+                                    model: models.users,
+                                    as: 'owners',
+                                    through: { attributes: [] }
+                              }]
+                        })
+                        if (!phone && args.ext) {
+                              throw new Error(`Phone record does not exist for extension: ${args.ext}`)
+                        }
+                        if (!phone && args.id) {
+                              throw new Error(`Phone record does not exist for id: ${args.id}`)
+                        }
+                        
+                        else {
+                              return (phone)
+                        }
+                  }
+                  catch (err) {
+                        return (err)
+                  }
+            },
+            /* -------- Phones Queries */
+            async allServiceRequests(root, args, { models }) {
+                  try {
+                        return models.service_requests.findAll()
+                  } catch (err) {
+                        return (err)
+                  }
+            },
+            async serviceRequest(root, { id }, { models }) {
+                  try {
+                        const sr = await models.service_requests.findByPk(id)
+                        if (!sr)
+                        {
+                              throw new Error(`Service Request record does not exist for id: ${id}`)
+                        } else {
+                              console.log(sr)
+                              return(sr)
+                        }
+                  } catch (err) {
+                        return (err)
+                  }
+            },
+      },
+      Mutation: {
+            async createUserForm(root, { input }, { models }) {
+                  try {
+                        const forms = await models.user_form.create(input)
+                        const pending_count = await models.user_form.count({ where: { status: 'pending' } })
+                        return { pending_count, forms }
+                  } catch (err) {
+                        return (err)
+                  }
+            },
+            async updateUserForm(root, { id, input }, { models }) {
+                  try {
+                        await models.user_form.update(input, {
+                              where: { id: id }
+
+                        })
+                        const form = await models.user_form.findByPk(id, {
+                              include:
+                              {
+                                    model: models.users,
+                                    as: 'submit_user'
+                              }
+                        })
+                        const pending_count = await models.user_form.count({ where: { status: 'pending' } })
+                        return { pending_count, form }
+                  } catch (err) {
+                        return (err)
+                  }
+            },
+            async createPhone(root, { input }, { models }) {
+                  try {
+                        const phone = await models.phones.create(input)
+                        return (phone)
+                  }
+                  catch (err) {
+                        return (err)
+                  }
+            },
+            async updatePhone(root, { id, input }, { models }) {
+                  try {
+                        await models.phones.update(input, {
+                              where: { id: id },
+                              include: [{
+                                    model: models.users,
+                                    as: 'owners',
+                                    through: { attributes: [] }
+                              }]
+                        })
+                        const phone = await models.phones.findOne({
+                              where: { id: id },
+                              include: [{
+                                    model: models.users,
+                                    as: 'owners',
+                                    through: { attributes: [] }
+                              }]
+                        })
+                        return (phone)
+                  }
+                  catch (err) {
+                        return (err)
+                  }
+            },
+            async deletePhone(root, { id }, { models }) {
+                  try {
+                        const phone = await models.phones.destroy({where: {id: id}})
+                        return ({message: `Phone ID: ${id} deleted.`})
+                  }
+                  catch (err) {
+                        return (err)
+                  }
+            },
+            async updatePhoneOwners(root, { id, owners }, { models }) {
+                  try {
+                        const getPhone = await models.phones.findOne({
+                              where: { id: id }
+
+                        });
+                        await getPhone.setOwners([owners])
+                        const newPhone = await models.phones.findOne({
+                              where: { id: id },
+                              include: [{
+                                    model: models.users,
+                                    as: 'owners',
+                                    through: { attributes: [] }
+                              }]
+                        })
+                        return (newPhone)
+                  }
+                  catch (err) {
+                        return (err)
+                  }
+            },
       },
 }
+
 
 module.exports = resolvers
--- src/schema.js
+++ src/schema.js
@@ -7,6 +7,9 @@ const {
 
 const typeDefs = gql`
     scalar DateTime
+    type Result {
+        message: String
+    }
     type Users {
         user_name: String!
         first_name: String
@@ -20,6 +23,15 @@ const typeDefs = gql`
         phones: [Phones!]
         pdq_computers: [Computers!]
         service_requests: [ServiceRequests!]
+    }
+    type DayForceUsers {
+        XRefCode: String
+        FirstName: String
+        LastName: String
+        Department: String
+        Title: String
+        Description: String
+        Manager:String
     }
     type OpenTickets {
         id: ID!
@@ -38,6 +50,14 @@ const typeDefs = gql`
         close_time: DateTime
     }
     type UserForms {
+        pending_count: Int!
+        forms: [uForms!]
+    }
+    type UserForm {
+        pending_count: Int!
+        form: uForms!
+    }
+    type uForms {
         id: ID!
         first_name: String
         last_name: String
@@ -71,8 +91,80 @@ const typeDefs = gql`
         needs_cell: Boolean
         pc_number: String
         start_date: String
+        submit_user: Users!
+        create_user: Users
+    }
+    input uFormInput {
+        first_name: String
+        last_name: String
+        display_name: String
+        user_name: String
+        employee_id: String 
+        job_title: String
+        building: String  
+        description: String
+        copy_user: String
+        create_mbx: Boolean
+        sup_man_execs: Boolean
+        home_drive: Boolean
+        submitted_by: String
+        created_by: String
+        status: String!
+        needs_computer: Boolean
+        needs_ax: Boolean
+        needs_ice: Boolean
+        needs_stellar: Boolean
+        needs_onbase: Boolean
+        needs_dl: Boolean
+        needs_scan: Boolean
+        needs_pdf: Boolean
+        needs_autocad: Boolean
+        needs_publisher: Boolean
+        needs_visio: Boolean
+        needs_shoretel: Boolean
+        needs_sec: Boolean
+        needs_deskphone: Boolean
+        needs_cell: Boolean
+        pc_number: String
+        start_date: String
     }
     type Phones {
+        id: ID!
+        full_number: String
+          telephone: String
+          division_id: Float
+          department: String
+          position: String
+          location: String
+          function_info: String
+          notes: String
+          account_number: String
+          date_installed: String
+          monthly_cost: String
+          investigate: Boolean
+          model: String
+          line_type: String
+          long_distance: String
+          need_voicemail: Boolean
+          disconnect_now: Boolean
+          disconnect_later: Boolean
+          phone_number: String
+          ld_changed: String
+          new_phone: String
+          switch_comments: String
+          new_location: String
+          drop_num: String
+          port_num: Float
+          extension: Int
+          vm_id: Int
+          binding_post: Float
+          provider: String
+          pin: String
+          date_created: String
+          owners: [Users!]
+    }
+    input uPhoneInput {
+
         full_number: String
           telephone: String
           division_id: Float
@@ -149,12 +241,34 @@ const typeDefs = gql`
         broadcast_address: String
     }
     type Query {
-        User(user_name: String!): Users
         allUsers: [Users!]!
+        User(user_name: String!): Users
+        dfUser(emp_id: String!): DayForceUsers
+
+        allUserForms(status: String, submitted_by: String): UserForms
+        UserForm(id: String!): UserForm
+
+        allComputers: [Computers!]!
+        Computer(host_name: String!): Computers
+
+        allPhones: [Phones!]!
+        Phone(id: ID, ext: Int): Phones
+
+        allServiceRequests: [ServiceRequests!]
+        serviceRequest(id: ID): ServiceRequests
+
+        
+
 
     }
     type Mutation {
-        createUser(user_name: String!, first_name: String!, last_name: String!): Users!
+        createUserForm(input: uFormInput): UserForm
+        updateUserForm(id: String!, input: uFormInput): UserForm
+        createPhone(input: uPhoneInput): Phones
+        updatePhone(id: String!, input: uPhoneInput): Phones
+        deletePhone(id: String!): Result
+        updatePhoneOwners(id: ID, owners: String): Phones
+
     }
 `
 
