bb954598c45acc8bfc2826ea1cc1e783de398a6f
dererererererer <derek@dpc-designs.com>
2020-02-12 09:14:46 -0800

    cors fix

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ const { ApolloServer } = require('apollo-server-express')
 const typeDefs = require('./schema')
 const resolvers = require('./resolvers')
 const models = require('../models')
+const cors = require('cors')
 
-const PORT = 4000;
-
+const PORT = process.env.PORT || 4000;
+const corsOptions = {
+  origin: ['http://192.168.235.97:4200', process.env.PROD_URL],
+  credentials: true,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 const app = express();
 
 const server = new ApolloServer({
@@ -13,6 +18,11 @@ const server = new ApolloServer({
   resolvers,
   context: { models }
 })
-server.applyMiddleware({ app })
+app.use(cors(corsOptions))
+server.applyMiddleware({ 
+  app,
+  path: '/',
+  cors: false
+ })
 app.listen({port: PORT }, () =>
 console.log(`Server is running on localhost:${PORT}${server.graphqlPath}`))
